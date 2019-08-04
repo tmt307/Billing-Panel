8b31b3360d614d8fc7f164d2ff8027a016908198
Thomas Turner <contact@mrthomasturner.com>
2019-08-04 20:23:00 +0100

    Update class.user.php

--- includes/class.user.php
+++ includes/class.user.php
@@ -113,17 +113,5 @@ class USER
 		$mail = new PHPMailer();
 		$mail->IsSMTP();
 		$mail->SMTPDebug  = 0;
-		$mail->SMTPAuth = true;							//Sets SMTP authentication. Utilizes the Username and Password variables
-		$mail->SMTPSecure = "ssl";
-		$mail->Host = 'server1.spartanhost.net';		//Sets the SMTP hosts of your Email hosting, this for Godaddy
-		$mail->Port = '465';								//Sets the default SMTP server port
-		$mail->AddAddress($email);
-		$mail->Username="no-reply@fitnessplanner.net";
-		$mail->Password="S.E;(u$.Z+1G";
-		$mail->SetFrom('no-reply@fitnessplanner.net','Fitness Planner');
-		$mail->AddReplyTo("no-reply@fitnessplanner.net","Fitness Planner");
-		$mail->Subject    = $subject;
-		$mail->MsgHTML($message);
-		$mail->Send();
-	}
+
 }
