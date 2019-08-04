9ff77285d88e8ece3f4398e7d9614f18db962d12
tmt307 <turnertm02@gmail.com>
2019-08-04 14:23:28 +0100

    login.register PHP intergration
    PHP intergration for register and login system

---
+++ includes/class.user.php
@@ -0,0 +1,129 @@
+<?php
+
+require_once 'includes/database-config.php';
+
+class USER
+{
+
+	private $conn;
+
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->dbConnection();
+		$this->conn = $db;
+    }
+
+	public function runQuery($sql)
+	{
+		$stmt = $this->conn->prepare($sql);
+		return $stmt;
+	}
+
+	public function lasdID()
+	{
+		$stmt = $this->conn->lastInsertId();
+		return $stmt;
+	}
+
+	public function register($username,$email,$upass,$code)
+	{
+		try
+		{
+			$password = md5($upass);
+			$stmt = $this->conn->prepare("INSERT INTO users(userName,userEmail,userPass,tokenCode)
+			 VALUES(:user_name, :user_mail, :user_pass, :active_code)");
+			$stmt->bindparam(":user_name",$username);
+			$stmt->bindparam(":user_mail",$email);
+			$stmt->bindparam(":user_pass",$password);
+			$stmt->bindparam(":active_code",$code);
+			$stmt->execute();
+			return $stmt;
+		}
+		catch(PDOException $ex)
+		{
+			echo $ex->getMessage();
+		}
+	}
+
+	public function login($email,$upass)
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT * FROM users WHERE userEmail=:email_id");
+			$stmt->execute(array(":email_id"=>$email));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+			if($stmt->rowCount() == 1)
+			{
+				if($userRow['userStatus']=="Y")
+				{
+					if($userRow['userPass']==md5($upass))
+					{
+						$_SESSION['userSession'] = $userRow['userID'];
+						return true;
+					}
+					else
+					{
+						header("Location: index.php?error");
+						exit;
+					}
+				}
+				else
+				{
+					header("Location: index.php?inactive");
+					exit;
+				}
+			}
+			else
+			{
+				header("Location: index.php?error");
+				exit;
+			}
+		}
+		catch(PDOException $ex)
+		{
+			echo $ex->getMessage();
+		}
+	}
+
+
+	public function is_logged_in()
+	{
+		if(isset($_SESSION['userSession']))
+		{
+			return true;
+		}
+	}
+
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+
+	public function logout()
+	{
+		session_destroy();
+		$_SESSION['userSession'] = false;
+	}
+
+	function send_mail($email,$message,$subject)
+	{
+		require_once('mailer/class.phpmailer.php');
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPDebug  = 0;
+		$mail->SMTPAuth = true;							//Sets SMTP authentication. Utilizes the Username and Password variables
+		$mail->SMTPSecure = "ssl";
+		$mail->Host = 'server1.spartanhost.net';		//Sets the SMTP hosts of your Email hosting, this for Godaddy
+		$mail->Port = '465';								//Sets the default SMTP server port
+		$mail->AddAddress($email);
+		$mail->Username="no-reply@fitnessplanner.net";
+		$mail->Password="hDB9ePaVGHRf";
+		$mail->SetFrom('no-reply@fitnessplanner.net','Fitness Planner');
+		$mail->AddReplyTo("no-reply@fitnessplanner.net","Fitness Planner");
+		$mail->Subject    = $subject;
+		$mail->MsgHTML($message);
+		$mail->Send();
+	}
+}
--- register.php
+++ register.php
@@ -1,66 +1,140 @@
-<?php require_once('includes/navigation-bar.php'); ?>
+<?php require_once('includes/header.php');
+
+require_once ('class.user.php');
+
+$reg_user = new USER();
+
+if($reg_user->is_logged_in()!="")
+{
+$reg_user->redirect('index.php');
+}
+
+
+if(isset($_POST['btn-register']))
+{
+$username = trim($_POST['username']);
+$email_address = trim($_POST['email_address']);
+$password = trim($_POST['password']);
+$code = md5(uniqid(rand()));
+
+$stmt = $reg_user->runQuery("SELECT * FROM users WHERE userEmail=:email_id");
+$stmt->execute(array(":email_id"=>$email_address));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if($stmt->rowCount() > 0)
+{
+$msg = "
+
+<div class='uk-alert-danger' uk-alert>
+<a class='uk-alert-close' uk-close></a>
+Email already exists , Please try diffrent one.
+</div>
+
+
+      ";
+}
+else
+{
+if($reg_user->register($username,$email_address,$password,$code))
+{
+    $id = $reg_user->lasdID();
+    $key = base64_encode($id);
+    $id = $key;
+
+    $message = "
+                Hello $username,
+                <br /><br />
+                Welcome to Fitness Planner!<br/>
+                To complete your registration  please , just click following link<br/>
+                <br /><br />
+                <a href='http://fitnessplanner.net/new/verify.php?id=$id&code=$code'> Click here to activate your account.</a>
+                <br /><br />
+                Thanks,";
+
+    $subject = "Confirm Registration";
+
+    $reg_user->send_mail($email_address,$message,$subject);
+    $msg = "
+
+<div class='uk-alert-success' uk-alert>
+<a class='uk-alert-close' uk-close></a>
+We've sent an email to $email_address.
+    Please click on the confirmation link in the email to create your account.
+</div>
+            ";
+}
+else
+{
+    echo "Opps, this is broken";
+}
+}
+}
+?>
+
+
 <div class="uk-section uk-section-secondary uk-flex uk-flex-middle uk-animation-fade" uk-height-viewport>
-    <div class="uk-width-1-1">
-        <div class="uk-container">
-            <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid>
-                <div class="uk-width-1-1@m">
-                    <div class="uk-margin uk-width-large uk-margin-auto uk-card uk-card-secondary uk-card-body uk-box-shadow-large">
-                        <h3 class="uk-card-title uk-text-center">Login Portal</h3>
-                        <form method="POST">
-                            <fieldset class="uk-fieldset">
+<div class="uk-width-1-1">
+<div class="uk-container">
+    <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid>
+        <div class="uk-width-1-1@m">
+            <div class="uk-margin uk-width-large uk-margin-auto uk-card uk-card-secondary uk-card-body uk-box-shadow-large">
+                <h3 class="uk-card-title uk-text-center">Login Portal</h3>
+                    <?php if(isset($msg)) echo $msg;  ?>
+                <form method="post">
+                    <fieldset class="uk-fieldset">
 
-                                <div class="uk-margin">
-                                    <div class="uk-position-relative">
-                                        <span class="uk-form-icon"  uk-icon="icon: user"></span>
-                                        <input name="email" class="uk-input" type="text" placeholder="Enter a username">
-                                    </div>
-                                </div>
+                        <div class="uk-margin">
+                            <div class="uk-position-relative">
+                                <span class="uk-form-icon"  uk-icon="icon: user"></span>
+                                <input name="username" class="uk-input" type="text" placeholder="Enter a username">
+                            </div>
+                        </div>
 
 
-                                <div class="uk-margin">
-                                    <div class="uk-position-relative">
-                                        <span class="uk-form-icon"  uk-icon="icon: mail"></span>
-                                        <input name="email" class="uk-input" type="email" placeholder="Enter an email address">
-                                    </div>
-                                </div>
+                        <div class="uk-margin">
+                            <div class="uk-position-relative">
+                                <span class="uk-form-icon"  uk-icon="icon: mail"></span>
+                                <input name="email_address" class="uk-input" type="email" placeholder="Enter an email address">
+                            </div>
+                        </div>
 
-    
 
-                                <div class="uk-margin">
-                                    <div class="uk-position-relative">
-                                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                                        <input type="password" name="password" class="uk-input" type="password" placeholder="Enter a password">
-                                    </div>
-                                </div>
 
-                            <div class="uk-margin">
-                                    <div class="uk-position-relative">
-                                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                                        <input type="password" name="password" class="uk-input" type="password" placeholder="Confirm your password">
-                                    </div>
-                                </div>
+                        <div class="uk-margin">
+                            <div class="uk-position-relative">
+                                <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                                <input type="password" name="password" class="uk-input" type="password" placeholder="Enter a password">
+                            </div>
+                        </div>
 
-                                <div class="uk-margin">
-                                           <button type="submit" class="uk-button uk-button-primary">
-                                  Register
-                                    </button>  <a href="" class="uk-align-right"> Forgot your password? </a>
-                                       </div>
-    
-                                    Already have an account?
-                                    <p>
-                                            <button type="submit" class="uk-button uk-button-default">
-                                  Login
-                                    </button> 
-                             </p>
-                                <hr />
-                        
-                                 
-                                </center>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
+                    <div class="uk-margin">
+                            <div class="uk-position-relative">
+                                <span class="uk-form-icon" u-icon="icon: lock"></span>
+                                <input type="password" name="confirm_password" class="uk-input" type="password" placeholder="Confirm your password">
+                            </div>
+                        </div>
+
+                        <div class="uk-margin">
+                                   <input type="submit" value="Register" name="btn-register" class="uk-button uk-button-primary">
+                
+                        <a href="" class="uk-align-right"> Forgot your password? </a>
+                               </div>
+
+                            Already have an account?
+                            <p>
+                                    <button type="submit" class="uk-button uk-button-default">
+                          Login
+                            </button> 
+                     </p>
+                        <hr />
+                
+                         
+                        </center>
+                    </fieldset>
+                </form>
             </div>
         </div>
     </div>
+</div>
+</div>
 </div>
