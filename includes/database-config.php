855b8c67d9106bcede65960ad3b7bca406f6c53e
Thomas Turner <contact@mrthomasturner.com>
2019-09-25 16:20:09 +0100

    Update database-config.php

--- includes/database-config.php
+++ includes/database-config.php
@@ -7,41 +7,6 @@ $pass = "root";
 $pdo = new PDO('mysql:host=localhost;dbname=billing', $user, $pass);
 
 
-class Database
-{
-  private $host = "localhost";
-  private $db_name = "test";
-  private $username = "test";
-  private $password = "";
-  private $db_name = "billing";
-  private $username = "root";
-  private $password = "root";
-    public $conn;
-
-    public function dbConnection()
-	{
-  {
-
-	    $this->conn = null;
-      $this->conn = null;
-        try
-		{
-    {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-
-			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-		catch(PDOException $exception)
-		{
-    catch(PDOException $exception)
-    {
-            echo "Connection Failed : " . $exception->getMessage();
-        }
-
-        return $this->conn;
-    }
-}
 
 
 ?>
