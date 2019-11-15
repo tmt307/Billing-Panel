07d9b900401dca79828b7763ff05322a5f725b2f
tmt307 <turnertm02@gmail.com>
2019-11-15 23:58:46 +0000

    updated to fix login bug and updated database matches

--- includes/database-config.php
+++ includes/database-config.php
@@ -1,7 +1,9 @@
 <?php $base_url = "http://localhost";
 $websitename = "Toms Billing System";
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $termsofservice = "Terms of service (also known as terms of use and terms and conditions, commonly abbreviated as TOS or ToS, ToU or T&C) are the legal agreements between a service provider and a person who wants to use that service. The person must agree to abide by the terms of service in order to use the offered service.";
+error_reporting(E_ALL);
 
 $accountfirstname="Thomas";
 $accountsurname="Turner";
