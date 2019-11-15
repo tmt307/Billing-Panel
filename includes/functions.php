b6561c373f67f23f791d3aca3cc4b368624fede1
tmt307 <turnertm02@gmail.com>
2019-11-15 23:19:38 +0000

    tidy up

--- includes/functions.php
+++ includes/functions.php
@@ -1,8 +1,5 @@
 <?php
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
 require_once 'database-config.php';
 
 class USER
@@ -29,30 +26,12 @@ class USER
 		return $stmt;
 	}
 
-
-	public function editaccount($username,$email)
-	{
-		try
-		{
-			$stmt = $this->conn->prepare("INSERT INTO account_users(userName,userEmail)
-			 VALUES(:user_name, :user_mail)");
-			$stmt->bindparam(":user_name",$username);
-			$stmt->bindparam(":user_mail",$email);
-			$stmt->execute();
-			return $stmt;
-		}
-		catch(PDOException $ex)
-		{
-			echo $ex->getMessage();
-		}
-	}
-
 	public function register($username,$email,$upass,$code)
 	{
 		try
 		{
 			$password = md5($upass);
-			$stmt = $this->conn->prepare("INSERT INTO account_users(userName,userEmail,userPass,tokenCode)
+			$stmt = $this->conn->prepare("INSERT INTO users(userName,userEmail,userPass,tokenCode)
 			 VALUES(:user_name, :user_mail, :user_pass, :active_code)");
 			$stmt->bindparam(":user_name",$username);
 			$stmt->bindparam(":user_mail",$email);
@@ -71,17 +50,17 @@ class USER
 	{
 		try
 		{
-			$stmt = $this->conn->prepare("SELECT * FROM account_users WHERE userEmail=:email_id");
+			$stmt = $this->conn->prepare("SELECT * FROM users WHERE userEmail=:email_id");
 			$stmt->execute(array(":email_id"=>$email));
 			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 
 			if($stmt->rowCount() == 1)
 			{
-				if($userRow['account_userstatus']=="Y")
+				if($userRow['userStatus']=="Y")
 				{
 					if($userRow['userPass']==md5($upass))
 					{
-						$_SESSION['account_usersession'] = $userRow['userID'];
+						$_SESSION['userSession'] = $userRow['userID'];
 						return true;
 					}
 					else
@@ -109,9 +88,33 @@ class USER
 	}
 
 
+public function login($email,$upass)
+{
+	try
+	{
+		$stmt = $this->conn->prepare("SELECT * FROM users WHERE userEmail=:email_id");
+		$stmt->execute(array(":email_id"=>$email));
+		$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+if($stmt->rowCount() == 1)
+{
+		if($userRow['userPass']==md5($upass))
+		{
+			$_SESSION['userSession'] = $userRow['userID'];
+			return true;
+		}	
+
+		}
+		catch(PDOException $ex)
+		{
+			echo $ex->getMessage();
+		}
+	}
+
+
 	public function is_logged_in()
 	{
-		if(isset($_SESSION['account_usersession']))
+		if(isset($_SESSION['userSession']))
 		{
 			return true;
 		}
@@ -125,7 +128,7 @@ class USER
 	public function logout()
 	{
 		session_destroy();
-		$_SESSION['account_usersession'] = false;
+		$_SESSION['userSession'] = false;
 	}
 
 	function send_mail($email,$message,$subject)
@@ -148,85 +151,3 @@ class USER
 		$mail->Send();
 	}
 }
-
-
-function table_fetch_rows($table, $where = '', $order_by = '', $limit_from = 0, $limit_to = 0)
-{
-	global $db;
-
-	$sql = sprintf('SELECT * FROM %s', $table);
-
-	if (strlen($where) > 0) {
-		$sql .= sprintf(' WHERE %s', $where);
-	}
-
-	if (strlen($order_by) > 0) {
-		$sql .= sprintf(' ORDER BY %s', $order_by);
-	}
-
-	if ($limit_from > 0 || $limit_to > 0) {
-		$sql .= sprintf(' LIMIT %d, %d', $limit_from, $limit_to);
-	}
-
-	$r = mysqli_query($db,$sql) or die(mysqli_error($db));
-	$rows = array();
-
-	while ($row = mysqli_fetch_assoc($r)) {
-		$rows[] = $row;
-	}
-
-	return $rows;
-}
-
-
-// PHP Image Upload Script 
- 
-    class imageUploader
-    {
-      const UPLOADS_FOLDER = '.';
-
-      static function upload()
-      {
-        $imagefile = $_FILES;
-
-        if( !isset($imagefile['files']['error']) )
-          throw new RuntimeException('Invalid parameters.');
-
-        //multiple uploads not permitted. you should queue file uploads from the client
-        if(is_array($imagefile['files']['error']))
-          throw new RuntimeException('Only one file allowed.');
-
-        switch ($imagefile['files']['error']) {
-          case UPLOAD_ERR_OK:
-            break;
-          case UPLOAD_ERR_NO_FILE:
-            throw new RuntimeException('No file sent.');
-          case UPLOAD_ERR_INI_SIZE:
-          case UPLOAD_ERR_FORM_SIZE:
-            throw new RuntimeException('Exceeded filesize limit.');
-          default:
-            throw new RuntimeException('Unknown errors.');
-        }
-
-        $max = ini_get('upload_max_filesize') * 1000 * 1000;
-        if ($imagefile['files']['size'] > $max)
-            throw new RuntimeException('Exceeded filesize limit.');
-
-        //check the file type - but not the one sent by the browser instead use finfo
-        $finfo = new finfo(FILEINFO_MIME_TYPE);
-        $mime = $finfo->file($_FILES['files']['tmp_name']);
-        $allowed = array('jpg' => 'image/jpeg', 'png' => 'image/png', 'gif' => 'image/gif');
-        $ext = array_search($mime, $allowed, true);
-
-        if(false === $ext)
-          throw new RuntimeException('Invalid file format.');
-
-        $path = sprintf( self::UPLOADS_FOLDER . '/%s.%s', sha1_file($imagefile['files']['tmp_name']), $ext );
-        if (!move_uploaded_file( $imagefile['files']['tmp_name'],$path))
-          throw new RuntimeException('Failed to move uploaded file.');
-        else
-          return true;
-      }
-    }
-    
-  
